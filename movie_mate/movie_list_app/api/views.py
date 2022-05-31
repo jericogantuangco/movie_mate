dcf0008608499a0ac3c0b39455042c77725b07cc
Jerico <jericogantuangco200@gmail.com>
2022-05-31 16:31:14 +0800

    added get and post functionality

--- movie_mate/movie_list_app/api/views.py
+++ movie_mate/movie_list_app/api/views.py
@@ -4,11 +4,21 @@ from movie_list_app.models import Movie
 from movie_list_app.api.serializers import MovieSerializer
 
 
-@api_view()
+@api_view(['GET', 'POST'])
 def movie_list(request):
-    movies = Movie.objects.all()
-    serializer = MovieSerializer(movies, many=True)
-    return Response(serializer.data)
+    
+    if request.method == 'GET':
+        movies = Movie.objects.all()
+        serializer = MovieSerializer(movies, many=True)
+        return Response(serializer.data)
+    
+    if request.method == 'POST':
+        serializer = MovieSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
 
 @api_view()
 def movie_details(response, pk):
