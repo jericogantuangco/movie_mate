23997684bde89a147c480d946345e0b67d4f3225
Jerico <jericogantuangco200@gmail.com>
2022-05-31 14:54:59 +0800

    initial commit

---
+++ movie_mate/movie_list_app/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from movie_list_app.models import Movie
+
+# Register your models here.
+admin.site.register(Movie)
---
+++ movie_mate/movie_list_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MovieListAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'movie_list_app'
