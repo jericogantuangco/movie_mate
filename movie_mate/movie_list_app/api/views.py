6291e359f817b372f8be36158e89795aa6b2e085
Jerico <jericogantuangco200@gmail.com>
2022-06-08 10:00:08 +0800

    added put and delete for movie details

--- movie_mate/movie_list_app/api/views.py
+++ movie_mate/movie_list_app/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework.response import Response
+from rest_framework import status
 from rest_framework.decorators import api_view
 from movie_list_app.models import Movie
 from movie_list_app.api.serializers import MovieSerializer
@@ -20,8 +21,37 @@ def movie_list(request):
         else:
             return Response(serializer.errors)
 
-@api_view()
-def movie_details(response, pk):
-    movie = Movie.objects.get(pk=pk)
-    serializer = MovieSerializer(movie)
-    return Response(serializer.data)
+@api_view(['GET', 'PUT', 'DELETE'])
+def movie_details(request, pk): 
+    
+    if request.method == 'GET':
+        
+        try:
+            movie = Movie.objects.get(pk=pk)
+        except Movie.DoesNotExist:
+            return Response(
+                {'Error': 'Movie not found'},
+                status=status.HTTP_404_NOT_FOUND
+            )
+        
+        serializer = MovieSerializer(movie) 
+        return Response(serializer.data)
+    
+    if request.method == 'PUT':
+        movie = Movie.objects.get(pk=pk)
+        serializer = MovieSerializer(movie, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        
+    if request.method == 'DELETE':
+        movie = Movie.objects.get(pk=pk)
+        movie.delete()
+        return Response(
+            status=status.HTTP_204_NO_CONTENT,
+        )
